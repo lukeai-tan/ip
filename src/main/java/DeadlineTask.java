01893690a49521654ccf0d9adb36904b7e6bf39b
lukeai-tan <lukeaidantan1612@gmail.com>
2025-08-30 16:25:27 +0800

    Update DeadlineTask.java
    Added getter function for deadline

--- src/main/java/DeadlineTask.java
+++ src/main/java/DeadlineTask.java
@@ -6,12 +6,16 @@ public class DeadlineTask extends Task {
         setDeadline(deadline);
     }
 
+    public String getDeadline() {
+        return deadline;
+    }
+
     public void setDeadline(String deadline) {
         this.deadline = deadline;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + deadline + ")";
+        return "[D]" + super.toString() + " (by: " + getDeadline() + ")";
     }
 }
