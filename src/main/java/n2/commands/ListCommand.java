b10b4463fc8c60ae5b5b5201461f8700bf6209ad
lukeai-tan <lukeaidantan1612@gmail.com>
2025-09-21 14:57:34 +0800

    Add JavaDoc to ListCommand

--- src/main/java/n2/commands/ListCommand.java
+++ src/main/java/n2/commands/ListCommand.java
@@ -3,18 +3,40 @@ package n2.commands;
 import n2.intellect.RedGirlsException;
 import n2.purpose.TaskList;
 
+/**
+ * Represents the {@code list} command, which displays all tasks currently
+ * stored in the {@link TaskList}.
+ *
+ * <p>When executed, it prints all tasks in order, along with their completion status.</p>
+ */
 public class ListCommand extends Command {
+    /**
+     * Creates a new {@code ListCommand} and validates the input. <br>
+     * The input must exactly match {@code "list"}.
+     *
+     * @param input raw user input
+     * @throws RedGirlsException if the input is not exactly "list"
+     */
     public ListCommand(String input) throws RedGirlsException {
         if (!input.equals("list")) {
             throw RedGirlsException.invalidListCommand();
         }
     }
 
+    /**
+     * Executes the {@code list} command. <br>
+     * Prints all tasks in {@link TaskList} to standard output.
+     */
     @Override
     public void execute() {
         TaskList.printList();
     }
 
+    /**
+     * Indicates that this command does not terminate the program.
+     *
+     * @return always {@code false}
+     */
     @Override
     public boolean isExit() {
         return false;
